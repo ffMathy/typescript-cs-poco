933db4aa0ce36fefe3cf2f2804c4eb9c7393b0d9
bfulljames <bfulljames@blizzard.com>
2015-11-19 18:42:45 -0800

    Basic working functionality

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-module.exports = function() {
-	return "Hello";
+var typeTranslation = {};
+
+typeTranslation["int"] = "number";
+typeTranslation["double"] = "number";
+typeTranslation["float"] = "number";
+typeTranslation["int32"] = "number";
+typeTranslation["int64"] = "number";
+typeTranslation["short"] = "number";
+
+var blockCommentRegex = new RegExp("/\\*([\\s\\S]*)\\*/", "gm");
+var lineCommentRegex = new RegExp("//(.*)", "g");
+var classRegex = /class ([\w\d]+)/;
+var propertyRegex = /public (\S*) ([\w\d]+)\s*{\s*get;\s*set;\s*}/gm;
+
+function removeComments(code) {
+    var output = code.replace(blockCommentRegex, "");
+
+    var lines = output.split("\n").map(function(line) {
+            return line.replace(lineCommentRegex, "");
+        });
+
+    return lines.join("\n");
+}
+
+module.exports = function(input) {
+    input = removeComments(input);
+
+    var className = classRegex.exec(input)[1];
+
+    var definition = 'interface ' + className + ' {\n';
+
+    var propertyResult;
+    
+    while (propertyResult = propertyRegex.exec(input)) {
+        var varType = typeTranslation[propertyResult[1]];
+
+        if (!varType) {
+            varType = propertyResult[1];
+        }
+
+        definition += '    ' + propertyResult[2] + ': ' + varType + ';\n';
+    }
+
+    definition += '}\n';
+
+    return definition;
 }
---
+++ spec/main.js
@@ -0,0 +1,51 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+var sampleFile = "\
+using System;\n\
+\n\
+namespace MyNamespace.Domain\n\
+{\n\
+    public class MyPoco\n\
+    {\n\
+        public MyPoco()\n\
+        {\n\
+        }\n\
+\n\
+        public MyPoco(RichObject value)\n\
+        {\n\
+            this.Id = value.Id;\n\
+            this.Name = value.Name;\n\
+            this.Title = value.Title;\n\
+        }\n\
+\
+        public int Id { get; set; }\n\
+        public string Name { get; set; }\n\
+        //public string IgnoreMe { get; set; }\n\
+        // public string IgnoreMe2 { get; set; }\n\
+        /* public string IgnoreMe3 {get; set; } */\n\
+        /*\n\
+        public string IgnoreMe4 {get; set; }\n\
+        */\n\
+        public string Title\n\
+        {\n\
+            get;\n\
+            set;\n\
+        }\n\
+    }\n\
+}\n";
+
+var expectedOutput = "interface MyPoco {\n\
+    Id: number;\n\
+    Name: string;\n\
+    Title: string;\n\
+}\n";
+
+var pocoGen = require('../index.js');
+
+describe('typescript-cs-poco', function() {
+	it('should transform a basic POCO correctly', function() {
+		var result = pocoGen(sampleFile);
+        
+        expect(result).toEqual(expectedOutput);
+	});
+});
